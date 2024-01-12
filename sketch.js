f443b3dd85f50a947878b18b820a41547a60e772
Albear-Yang <albert.yang727@gmail.com>
2024-01-11 22:18:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -225,7 +225,7 @@ function getAvg(a, b, c, d, e, f, g, h, i){
 let map = [];
 p.setup = function(){
     p.frameRate(12);
-    p.createCanvas(Math.max(400, Math.min(p.windowWidth / 2, p.windowHeight / 2)), Math.max(400, Math.min(p.windowWidth / 2, p.windowHeight / 2)));
+    p.createCanvas(400, 400);
 
     p.background(0);
     p.pixelDensity(1);
